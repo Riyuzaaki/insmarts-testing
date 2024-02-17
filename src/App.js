1cb52bb3e059a797129cb1755365e6455f8a3e7c
Firoz Ahmed Hazari <firoz.ahmed@workindia.in>
2024-02-18 01:59:56 +0530

    mfknhjnjg

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import SearchResults from './pages/SearchResults';
-import History from './pages/History';
-import Title  from './pages/Title';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SearchResults from "./pages/SearchResults";
+import History from "./pages/History";
+import Title from "./pages/Title";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-      <Route exact path="/" element={<Title />} />
-        <Route exact path="/search" element={<SearchResults />} />
-        <Route exact path="/history" element={<History />} />
+        <Route exact path="insmarts-testing/" element={<Title />} />
+        <Route
+          exact
+          path="insmarts-testing/search"
+          element={<SearchResults />}
+        />
+        <Route exact path="insmarts-testing/history" element={<History />} />
       </Routes>
     </BrowserRouter>
   );
